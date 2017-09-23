5b198e8ee3f6baefe462d94885f23c3aa7432cb6
daniel <danielsollis2@gmail.com>
2017-09-23 14:26:09 -0700

    coffeeRun Works

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,15 @@
 (function(window) {
     'use strict';
+    var FORM_SELECTOR = '[data-coffee-order]';
     var App = window.App;
     var Truck = App.Truck;
     var Datastore = App.Datastore;
+    var FormHandler = App.FormHandler;
     var myTruck = new Truck('ncc-1701', new Datastore());
     window.myTruck = myTruck;
+    var formHandler = new FormHandler(FORM_SELECTOR);
+
+    formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+    console.log(formHandler);
+
 })(window);
