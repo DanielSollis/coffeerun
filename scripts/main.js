a243cdd528e23b9d13fa2f8385674855ea5e0b18
daniel <danielsollis2@gmail.com>
2017-09-30 09:05:20 -0700

    finished RemoteDataStore

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,18 @@
 (function(window) {
     'use strict';
     var FORM_SELECTOR = '[data-coffee-order]';
+    var SERVER_URL = 'http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders';
     var App = window.App;
     var Truck = App.Truck;
+    var RemoteDataStore = App.RemoteDataStore;
     var Datastore = App.Datastore;
+    var remoteDS = new RemoteDataStore(SERVER_URL);
+    var Validation = App.Validation;
     var FormHandler = App.FormHandler;
-    var myTruck = new Truck('ncc-1701', new Datastore());
+    var myTruck = new Truck('ncc-1701', remoteDS);
     window.myTruck = myTruck;
     var formHandler = new FormHandler(FORM_SELECTOR);
-
+    formHandler.addInputHandler(Validation.isCompanyEmail);
     formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
     console.log(formHandler);
 
